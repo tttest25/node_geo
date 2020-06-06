469a6b4d5dc848b7a04d728c2857760064bcae2b
mea <emelnikoff@gmail.com>
2020-06-06 10:03:08 +0500

    add .env

--- static.js
+++ static.js
@@ -12,6 +12,11 @@ const url = require('url');
 
 const fs = require('fs');
 
+const apiKey = process.env.API_KEY;
+console.log('Api key: '+apiKey);
+
+
+
 function pDownload(pUrl, dest) {
   const vfile = fs.createWriteStream(dest);
   return new Promise((resolve, reject) => {
@@ -38,30 +43,30 @@ http.createServer((req, res) => {
   const path = urlParsed.pathname;
   if (path === '/update') {
     // Table
-    pDownload('https://sheets.googleapis.com/v4/spreadsheets/1bauKrfVdg2nnJLO_TUVvG_tLfWjvRJRu16klsWu1XcM/values/objects!A1:J5000?key=AIzaSyB1HGxx5hlRpyXkBGykB69XgiLRpD7e5Ro', './static/json/objects.json')
+    pDownload(`https://sheets.googleapis.com/v4/spreadsheets/1bauKrfVdg2nnJLO_TUVvG_tLfWjvRJRu16klsWu1XcM/values/objects!A1:J5000?key=${apiKey}`, './static/json/objects.json')
     /*  .then(() => {
         console.log('district - downloaded file no issues...')
-        return pDownload('https://sheets.googleapis.com/v4/spreadsheets/1ShswXuI-IhGlR1TbDXQbtSF7C2QshLQHQcEIwf38Hao/values/district!A2:I475?key=AIzaSyB1HGxx5hlRpyXkBGykB69XgiLRpD7e5Ro', './static/json/district.json');
+        return pDownload('https://sheets.googleapis.com/v4/spreadsheets/1ShswXuI-IhGlR1TbDXQbtSF7C2QshLQHQcEIwf38Hao/values/district!A2:I475?key=', './static/json/district.json');
       })// uik
       .then(() => {
         console.log('uik - downloaded file no issues...');
-        return pDownload('https://sheets.googleapis.com/v4/spreadsheets/16mEcbu8lQA9PpzB6TO9igYALVYkUJYmf0a5BMHaQFdA/values/uik!A2:j1250?key=AIzaSyB1HGxx5hlRpyXkBGykB69XgiLRpD7e5Ro', './static/json/uik.json');
+        return pDownload('https://sheets.googleapis.com/v4/spreadsheets/16mEcbu8lQA9PpzB6TO9igYALVYkUJYmf0a5BMHaQFdA/values/uik!A2:j1250?key=', './static/json/uik.json');
       })// meropr
       .then(() => {
         console.log('meropr - downloaded file no issues...');
-        return pDownload('https://sheets.googleapis.com/v4/spreadsheets/16mEcbu8lQA9PpzB6TO9igYALVYkUJYmf0a5BMHaQFdA/values/meropr!A2:I178?key=AIzaSyB1HGxx5hlRpyXkBGykB69XgiLRpD7e5Ro', './static/json/meropr.json');
+        return pDownload('https://sheets.googleapis.com/v4/spreadsheets/16mEcbu8lQA9PpzB6TO9igYALVYkUJYmf0a5BMHaQFdA/values/meropr!A2:I178?key=', './static/json/meropr.json');
       })// Ansamb
       .then(() => {
         console.log('Ansamb - downloaded file no issues...');
-        return pDownload('https://sheets.googleapis.com/v4/spreadsheets/16mEcbu8lQA9PpzB6TO9igYALVYkUJYmf0a5BMHaQFdA/values/ansam!A2:I29?key=AIzaSyB1HGxx5hlRpyXkBGykB69XgiLRpD7e5Ro', './static/json/ansam.json');
+        return pDownload('https://sheets.googleapis.com/v4/spreadsheets/16mEcbu8lQA9PpzB6TO9igYALVYkUJYmf0a5BMHaQFdA/values/ansam!A2:I29?key=', './static/json/ansam.json');
       })// agit
       .then(() => {
         console.log('agit - downloaded file no issues...');
-        return pDownload('https://sheets.googleapis.com/v4/spreadsheets/16mEcbu8lQA9PpzB6TO9igYALVYkUJYmf0a5BMHaQFdA/values/agit!A2:I29?key=AIzaSyB1HGxx5hlRpyXkBGykB69XgiLRpD7e5Ro', './static/json/agit.json');
+        return pDownload('https://sheets.googleapis.com/v4/spreadsheets/16mEcbu8lQA9PpzB6TO9igYALVYkUJYmf0a5BMHaQFdA/values/agit!A2:I29?key=', './static/json/agit.json');
       })// reserve
       .then(() => {
         console.log('reserve - downloaded file no issues...');
-        return pDownload('https://sheets.googleapis.com/v4/spreadsheets/16mEcbu8lQA9PpzB6TO9igYALVYkUJYmf0a5BMHaQFdA/values/reserve!A2:I170?key=AIzaSyB1HGxx5hlRpyXkBGykB69XgiLRpD7e5Ro', './static/json/reserve.json');
+        return pDownload('https://sheets.googleapis.com/v4/spreadsheets/16mEcbu8lQA9PpzB6TO9igYALVYkUJYmf0a5BMHaQFdA/values/reserve!A2:I170?key=', './static/json/reserve.json');
       })   
       */
       .then(() => {console.log('DATA - downloaded file no issues...')
